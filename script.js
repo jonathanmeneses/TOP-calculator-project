df968fb6374c000aec053824bc8e194e8398ac90
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-10-21 20:20:33 -0500

    working on operators

--- script.js
+++ script.js
@@ -21,6 +21,55 @@ function divide(a,b) {
         return a/b}
 }
 
-function operate(operator,a,b) {
-    return operator(a,b)
+function operate(operator) {
+    currentResult = operator(Number(currentResult),Number(workingString))
+    workingString = ''
+    resultDisplay.innerHTML = currentResult
+    operator =''
+
 }
+
+
+// Creating Elements
+
+// Define initial variables
+
+let currentResult = 0;
+let workingString ='0';
+let operator = add
+let resultDisplay = document.querySelector("#result-text")
+
+function inputUpdate(event) {
+    console.log(`clicked ${event.target.id}`)
+    workingString = Number(workingString + event.target.id)
+    resultDisplay.innerHTML = workingString
+
+}
+
+
+// Add Event Listeners to update workingString for each number selected
+let numberButtons = document.querySelectorAll('.number')
+
+for (let i=0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', inputUpdate)
+}
+
+// Add equals Functionality
+// If no operator previously selected just show the working operator
+
+
+let equalButton = document.querySelector("#equals")
+equalButton.addEventListener('click',equals)
+
+function equals() {
+    if (operator = '') {
+        currentResult = workingString
+        resultDisplay.innerHTML = currentResult
+        workingString = ''
+    }
+    else {
+        operate(operator)
+    }
+}
+
+
